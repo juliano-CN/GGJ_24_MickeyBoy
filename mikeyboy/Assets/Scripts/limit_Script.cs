4b09090fafd70a25dbfc7ac262b9b72b9e095fbc
Narto Junior <nartojunior@outlook.com>
2024-01-28 13:04:14 -0300

    feat(street): adiciona painel de UI.

--- mikeyboy/Assets/Scripts/limit_Script.cs
+++ mikeyboy/Assets/Scripts/limit_Script.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class limit_Script : MonoBehaviour
 {
@@ -13,21 +14,49 @@ public class limit_Script : MonoBehaviour
     [SerializeField]
     mouse_test player;
     public Text textHP;
+    [SerializeField] GameObject UIScore;
+    [SerializeField] SpawnHouse spawnLeft, spawnRigth;
+    [SerializeField] bool canCount = true;
+
+    private void Start()
+    {
+        if (UIScore == null)
+        {
+            Debug.LogError("Precisa setar objecto com UI Score .");
+        }
+
+        if (spawnLeft == null || spawnRigth == null)
+        {
+            Debug.LogError("Precisa setar os Objectos de Spawn Left e Right.");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "house")
+        if(collision.tag == "house" && canCount)
         {
-            player.hp -= 1;
             Destroy(collision.gameObject);
-            perderPontoAudio.Play();
-            textHP.text = player.hp.ToString();
-            if(player.hp <= 0)
+
+            if (canCount)
             {
-                inventory.lastScore = inventory.score;
-                inventory.score += player.GetComponent<points>().score;
-                SceneManager.LoadScene("BakedGoodsStore");
-            }
+                player.hp -= 1;
+                perderPontoAudio.Play();
+                textHP.text = player.hp.ToString();
+
+                if (player.hp <= 0)
+                {
+                    inventory.lastScore = inventory.score;
+                    inventory.score += player.GetComponent<points>().score;
+
+                    spawnLeft.canSpawn = false;
+                    spawnRigth.canSpawn = false;
+
+                    UIScore.SetActive(true);
+                    canCount = false;
+
+                    //SceneManager.LoadScene("BakedGoodsStore");
+                }
+            }                      
         }
     }
 }
